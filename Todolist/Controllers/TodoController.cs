6fd83b1cfad0644efb08e641a4ddf465013ad0b0
Zain <syed.muhammad.zain.akbber@gmail.com>
2024-05-15 11:19:57 +0500

    CURD implemented

--- Todolist/Controllers/TodoController.cs
+++ Todolist/Controllers/TodoController.cs
@@ -22,6 +22,7 @@ namespace Todolist.Controllers
 		{
 			return View();
 		}
+
 		[HttpPost]
 		public IActionResult Create(Todo obj)
 		{
@@ -30,9 +31,63 @@ namespace Todolist.Controllers
 
 				_db.todos.Add(obj);
 				_db.SaveChanges();
+				TempData["success"] = "Task Created Successfully";
 				return RedirectToAction("Index");
 			}
 			return View();
+		}
+		public IActionResult Edit(int? id)
+		{
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+			Todo? todoData = _db.todos.Find(id);
+			if (todoData == null)
+			{
+				return NotFound();
+			}
+			return View(todoData);
+		}
+
+		[HttpPost]
+		public IActionResult Edit(Todo obj)
+		{
+			if (ModelState.IsValid)
+			{
+				_db.todos.Update(obj);
+				_db.SaveChanges();
+				TempData["success"] = "Task Updated Successfully";
+				return RedirectToAction("Index");
+			}
+			return View();
+		}
+		public IActionResult Delete(int? id)
+		{
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+			Todo? todoData = _db.todos.Find(id);
+			if (todoData == null)
+			{
+				return NotFound();
+			}
+			return View(todoData);
+		}
+
+		[HttpPost, ActionName("Delete")]
+		public IActionResult DeletePOST(int? id)
+		{
+			Todo? todo = _db.todos.Find(id);
+			if (todo == null)
+			{
+				return NotFound();
+			}
+			_db.todos.Remove(todo);
+			_db.SaveChanges();
+			TempData["success"] = "Task Deleted Successfully";
+			return RedirectToAction("Index");
 		}
 	}
 }
